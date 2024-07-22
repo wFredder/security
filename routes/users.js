a9ce225cfa8412ade60250f70b71d634066662b3
Josemiu <josemigueldelgadob@yahoo.com>
2024-07-22 10:53:46 -0500

    roles

--- routes/users.js
+++ routes/users.js
@@ -16,8 +16,10 @@ var router = express.Router();
   /* 3. Uso del método findAll */
   let usersCollection = await models.users.findAll({ })
 
+  let rolesCollection = await models.roles.findAll({ })
+
   /* 4. Paso de parámetros a la vista */
-  res.render('crud', { title: 'CRUD with users', usersArray: usersCollection });
+  res.render('crud', { title: 'CRUD with users', usersArray: usersCollection, rolesArray: rolesCollection });
 
 });
 
