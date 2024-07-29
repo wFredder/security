be19218031699f769a6b028021222fcd3dee47ec
Josemiu <josemigueldelgadob@yahoo.com>
2024-07-29 10:50:25 -0500

    parcial

--- routes/index.js
+++ routes/index.js
@@ -57,6 +57,10 @@ router.post('/login', async function (req, res, next) {
           /* 2. Cree la cookie 'username' con la variable user y la configuración de options  */
           res.cookie("username", username, options)
 
+          /* 1. Habilite la sesión */
+          req.session.loggedin = true;
+          req.session.username = username;
+
           /* 10. En caso de éxito, redirija a '/users' */
           res.redirect('/users');
         } else {
@@ -75,6 +79,13 @@ router.post('/login', async function (req, res, next) {
     res.redirect('/');
   }
 
+});
+
+/* GET logout. */
+/* 2. Método para terminar la sesión */
+router.get('/logout', function (req, res, next) {
+  req.session.destroy();
+  res.render('index');
 });
 
 module.exports = router;
